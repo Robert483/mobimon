5aae31b2b91286ce3b00bf9893a7577226ffd5a6
Marcus Edward <phantom.lancer.z@gmail.com>
2016-05-24 01:06:36 +0700

    Khoi tao project voi file README

---
+++ app/src/main/java/com/alarmnotification/mobimon/SplashArt.java
@@ -0,0 +1,13 @@
+package com.alarmnotification.mobimon;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashArt extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_art);
+    }
+}
